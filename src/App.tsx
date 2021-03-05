17bd5fa3fd4cb8785385564945867acb81a12de3
JLarky <jlarky@gmail.com>
2021-03-05 15:24:21 -0800

    implementation (about 45 min)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,90 @@
 import React from "react";
 
-export const App: React.FC = (props) => {
+/*
+Given an user input, display a message if the input can be typed using letters of only one row on a keyboard. 
+
+Extra credit: Include the option for a user to pick the type of keyboard they are using (querty, azerty, etc)!
+
+Example: (Using QWERTY - US Layout https://en.wikipedia.org/wiki/QWERTY)
+  isValid(‘candy’)
+  isValid(‘doodle’)
+  isValid(‘pop’)             // True
+  isValid(‘shield’)
+  isValid(‘lag’)               // True
+  isValid(‘typewriter’)  // True
+*/
+
+const types = ["querty", "azerty", "zwerty"] as const;
+type KeyboardType = typeof types[number];
+
+const keyDB = new Map<KeyboardType, string[]>();
+keyDB.set("querty", ["qwertyuiop", "asdfghjkl", "zxcvbnm"]);
+keyDB.set("azerty", ["azertyuiop", "qsdfghjklm", "wxcvbn"]);
+keyDB.set("zwerty", ["qwertzuiop", "asdfghjkl", "yxcvbnm"]);
+
+function isValid(str: string, type: KeyboardType = "querty") {
+  const keys = keyDB.get(type);
+  if (keys === undefined) {
+    throw new Error(`wrong keyboard type ${type}`);
+  }
+  let rowUsed: number | undefined = undefined;
+  const hasMultipleRows = str.split("").some((key) => {
+    const rowIndex = keys.findIndex((row) => row.match(key));
+    if (rowUsed === undefined) {
+      rowUsed = rowIndex;
+    } else {
+      if (rowIndex !== rowUsed) {
+        // we have new row that wasn't used before, so we need to exit here
+        return true;
+      }
+    }
+    return false;
+  });
+  return !hasMultipleRows;
+}
+
+function isKbType(type: string): type is KeyboardType {
+  return types.includes(type as KeyboardType);
+}
+
+export const App: React.FC = () => {
+  const [keyboardType, setKeyboardType] = React.useState<KeyboardType>(
+    "querty"
+  );
+  const [inputValue, setInputValue] = React.useState("");
+  const valid = React.useMemo(() => isValid(inputValue, keyboardType), [
+    inputValue,
+    keyboardType,
+  ]);
+
   return (
-    <div className="bg-gray-800">
-      <header className="flex min-h-screen items-center justify-center text-gray-200">
-        <p>
-          Edit <code className="font-bold">src/App.tsx</code> and save to
-          reload.
-        </p>
-      </header>
+    <div className="m-10">
+      <h1 className="text-2xl my-8">
+        Type something and we will see if that's easy to type
+      </h1>
+      <input
+        type="text"
+        className="border"
+        onChange={(e) => {
+          setInputValue(e.currentTarget.value);
+        }}
+      />
+      <select
+        className="uppercase"
+        onChange={(e) => {
+          const newValue = e.target.value;
+          if (isKbType(newValue)) {
+            setKeyboardType(newValue);
+          }
+        }}
+      >
+        {types.map((key) => {
+          return <option key={key}>{key}</option>;
+        })}
+      </select>
+      <div className="my-4 text-red-800">
+        {valid ? "" : "You are using too many keyboard rows!"}
+      </div>
     </div>
   );
 };
